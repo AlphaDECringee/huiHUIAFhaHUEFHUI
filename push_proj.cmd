729b3ba54875aaa57e852a8370dbb0e369e54570
Niklas Krumpe <alphadevia11@gmail.com>
2023-05-28 01:25:41 +0200

    Erster Commit

---
+++ push_proj.cmd
@@ -0,0 +1,8 @@
+git add .
+git config --global user.mail "alphadevia11@gmail.com"
+git config --global user.name "Niklas Krumpe"
+git commit -m "Erster Commit"
+git remote add origin https://github.com/AlphaDECringee/huiHUIAFhaHUEFHUI
+git branch -m main
+git push -u origin main
+PAUSE
