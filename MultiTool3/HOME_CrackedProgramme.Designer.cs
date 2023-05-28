31c7b26f3e0b557d629f6d0cc319ef9f6875c059
Niklas Krumpe <alphadevia11@gmail.com>
2023-05-28 18:15:11 +0200

    Erster Commit

--- MultiTool3/HOME_CrackedProgramme.Designer.cs
+++ MultiTool3/HOME_CrackedProgramme.Designer.cs
@@ -32,11 +32,25 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.BTN_Filmora9 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BTN_IDM = new System.Windows.Forms.Button();
+            this.BTN_iObitASCP = new System.Windows.Forms.Button();
+            this.BTN_iObitDBP = new System.Windows.Forms.Button();
+            this.BTN_iObitMFP = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.BTN_iObitSDP = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.BTN_iObitUP = new System.Windows.Forms.Button();
+            this.BTN_WinRAR = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BTN_4KVD
             // 
-            this.BTN_4KVD.Location = new System.Drawing.Point(113, 0);
+            this.BTN_4KVD.Location = new System.Drawing.Point(194, 0);
             this.BTN_4KVD.Name = "BTN_4KVD";
             this.BTN_4KVD.Size = new System.Drawing.Size(75, 23);
             this.BTN_4KVD.TabIndex = 0;
@@ -64,7 +78,7 @@
             // 
             // BTN_Filmora9
             // 
-            this.BTN_Filmora9.Location = new System.Drawing.Point(113, 23);
+            this.BTN_Filmora9.Location = new System.Drawing.Point(194, 23);
             this.BTN_Filmora9.Name = "BTN_Filmora9";
             this.BTN_Filmora9.Size = new System.Drawing.Size(75, 23);
             this.BTN_Filmora9.TabIndex = 3;
@@ -72,10 +86,157 @@
             this.BTN_Filmora9.UseVisualStyleBackColor = true;
             this.BTN_Filmora9.Click += new System.EventHandler(this.BTN_Filmora9_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-3, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "IDM";
+            // 
+            // BTN_IDM
+            // 
+            this.BTN_IDM.Location = new System.Drawing.Point(194, 46);
+            this.BTN_IDM.Name = "BTN_IDM";
+            this.BTN_IDM.Size = new System.Drawing.Size(75, 23);
+            this.BTN_IDM.TabIndex = 5;
+            this.BTN_IDM.Text = "Download";
+            this.BTN_IDM.UseVisualStyleBackColor = true;
+            this.BTN_IDM.Click += new System.EventHandler(this.BTN_IDM_Click);
+            // 
+            // BTN_iObitASCP
+            // 
+            this.BTN_iObitASCP.Location = new System.Drawing.Point(194, 69);
+            this.BTN_iObitASCP.Name = "BTN_iObitASCP";
+            this.BTN_iObitASCP.Size = new System.Drawing.Size(75, 23);
+            this.BTN_iObitASCP.TabIndex = 6;
+            this.BTN_iObitASCP.Text = "Download";
+            this.BTN_iObitASCP.UseVisualStyleBackColor = true;
+            this.BTN_iObitASCP.Click += new System.EventHandler(this.BTN_iObitASCP_Click);
+            // 
+            // BTN_iObitDBP
+            // 
+            this.BTN_iObitDBP.Location = new System.Drawing.Point(194, 92);
+            this.BTN_iObitDBP.Name = "BTN_iObitDBP";
+            this.BTN_iObitDBP.Size = new System.Drawing.Size(75, 23);
+            this.BTN_iObitDBP.TabIndex = 7;
+            this.BTN_iObitDBP.Text = "Download";
+            this.BTN_iObitDBP.UseVisualStyleBackColor = true;
+            this.BTN_iObitDBP.Click += new System.EventHandler(this.BTN_iObitDBP_Click);
+            // 
+            // BTN_iObitMFP
+            // 
+            this.BTN_iObitMFP.Location = new System.Drawing.Point(194, 115);
+            this.BTN_iObitMFP.Name = "BTN_iObitMFP";
+            this.BTN_iObitMFP.Size = new System.Drawing.Size(75, 23);
+            this.BTN_iObitMFP.TabIndex = 8;
+            this.BTN_iObitMFP.Text = "Download";
+            this.BTN_iObitMFP.UseVisualStyleBackColor = true;
+            this.BTN_iObitMFP.Click += new System.EventHandler(this.BTN_iObitMFP_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-3, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "iObit Smart Defrag Pro 8";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(-3, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "iObit Malware Fighter Pro 9";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(-3, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "iObit Driver Booster Pro 9";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(-3, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(173, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "iObit Advanced SystemCare Pro 15";
+            // 
+            // BTN_iObitSDP
+            // 
+            this.BTN_iObitSDP.Location = new System.Drawing.Point(194, 138);
+            this.BTN_iObitSDP.Name = "BTN_iObitSDP";
+            this.BTN_iObitSDP.Size = new System.Drawing.Size(75, 23);
+            this.BTN_iObitSDP.TabIndex = 13;
+            this.BTN_iObitSDP.Text = "Download";
+            this.BTN_iObitSDP.UseVisualStyleBackColor = true;
+            this.BTN_iObitSDP.Click += new System.EventHandler(this.BTN_iObitSDP_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(-3, 166);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "iObit Uninstaller Pro 11";
+            // 
+            // BTN_iObitUP
+            // 
+            this.BTN_iObitUP.Location = new System.Drawing.Point(194, 161);
+            this.BTN_iObitUP.Name = "BTN_iObitUP";
+            this.BTN_iObitUP.Size = new System.Drawing.Size(75, 23);
+            this.BTN_iObitUP.TabIndex = 15;
+            this.BTN_iObitUP.Text = "Download";
+            this.BTN_iObitUP.UseVisualStyleBackColor = true;
+            this.BTN_iObitUP.Click += new System.EventHandler(this.BTN_iObitUP_Click);
+            // 
+            // BTN_WinRAR
+            // 
+            this.BTN_WinRAR.Location = new System.Drawing.Point(194, 184);
+            this.BTN_WinRAR.Name = "BTN_WinRAR";
+            this.BTN_WinRAR.Size = new System.Drawing.Size(75, 23);
+            this.BTN_WinRAR.TabIndex = 16;
+            this.BTN_WinRAR.Text = "Download";
+            this.BTN_WinRAR.UseVisualStyleBackColor = true;
+            this.BTN_WinRAR.Click += new System.EventHandler(this.BTN_WinRAR_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(-3, 189);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "WinRAR";
+            // 
             // HOME_CrackedProgramme
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.BTN_WinRAR);
+            this.Controls.Add(this.BTN_iObitUP);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.BTN_iObitSDP);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BTN_iObitMFP);
+            this.Controls.Add(this.BTN_iObitDBP);
+            this.Controls.Add(this.BTN_iObitASCP);
+            this.Controls.Add(this.BTN_IDM);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.BTN_Filmora9);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -93,5 +254,19 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button BTN_Filmora9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BTN_IDM;
+        private System.Windows.Forms.Button BTN_iObitASCP;
+        private System.Windows.Forms.Button BTN_iObitDBP;
+        private System.Windows.Forms.Button BTN_iObitMFP;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button BTN_iObitSDP;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button BTN_iObitUP;
+        private System.Windows.Forms.Button BTN_WinRAR;
+        private System.Windows.Forms.Label label9;
     }
 }
--- MultiTool3/HOME_CrackedProgramme.cs
+++ MultiTool3/HOME_CrackedProgramme.cs
@@ -27,5 +27,68 @@ namespace MultiTool3
             DownloadFile dwnDiag = new DownloadFile(fourKVD, url, oFile);
             dwnDiag.ShowDialog();
         }
+
+        private void BTN_IDM_Click(object sender, EventArgs e)
+        {
+            string oFile = "InternetDownloadManager.zip";
+            string fourKVD = "Internet Download Manager";
+            string url = "https://github.com/AlphaDECringee/softwarerepository/releases/download/idm/InternetDownloadManager.zip";
+            DownloadFile dwnDiag = new DownloadFile(fourKVD, url, oFile);
+            dwnDiag.ShowDialog();
+        }
+
+        private void BTN_iObitASCP_Click(object sender, EventArgs e)
+        {
+            string oFile = "Advanced_SystemCare_Pro_15.2.0.201.zip";
+            string fourKVD = "iObit Advanced SystemCare Pro 15.2.0.201";
+            string url = "https://github.com/AlphaDECringee/softwarerepository/releases/download/ASCP/Advanced_SystemCare_Pro_15.2.0.201.zip";
+            DownloadFile dwnDiag = new DownloadFile(fourKVD, url, oFile);
+            dwnDiag.ShowDialog();
+        }
+
+        private void BTN_iObitDBP_Click(object sender, EventArgs e)
+        {
+            string oFile = "IObit_Driver_Booster_Pro_9.2.0.178.zip";
+            string fourKVD = "iObit Driver Booster Pro 15.2.0.201";
+            string url = "https://github.com/AlphaDECringee/softwarerepository/releases/download/DBP/IObit_Driver_Booster_Pro_9.2.0.178.zip";
+            DownloadFile dwnDiag = new DownloadFile(fourKVD, url, oFile);
+            dwnDiag.ShowDialog();
+        }
+
+        private void BTN_iObitMFP_Click(object sender, EventArgs e)
+        {
+            string oFile = "IObit_Malware_Fighter_Pro_9.1.0.553.zip";
+            string fourKVD = "iObit Malware Fighter Pro 9.1.0.553";
+            string url = "https://github.com/AlphaDECringee/softwarerepository/releases/download/MFP/IObit_Malware_Fighter_Pro_9.1.0.553.zip";
+            DownloadFile dwnDiag = new DownloadFile(fourKVD, url, oFile);
+            dwnDiag.ShowDialog();
+        }
+
+        private void BTN_iObitSDP_Click(object sender, EventArgs e)
+        {
+            string oFile = "IObit_Smart_Defrag_Pro_8.0.0.136.zip";
+            string fourKVD = "iObit Smart Defrag Pro 8.0.0.136";
+            string url = "https://github.com/AlphaDECringee/softwarerepository/releases/download/SDP/IObit_Smart_Defrag_Pro_8.0.0.136.zip";
+            DownloadFile dwnDiag = new DownloadFile(fourKVD, url, oFile);
+            dwnDiag.ShowDialog();
+        }
+
+        private void BTN_iObitUP_Click(object sender, EventArgs e)
+        {
+            string oFile = "IObit_Uninstaller_Pro_11.3.0.4_Multilingual.zip";
+            string fourKVD = "iObit Uninstaller Pro 11.3.0.4";
+            string url = "https://github.com/AlphaDECringee/softwarerepository/releases/download/UP/IObit_Uninstaller_Pro_11.3.0.4_Multilingual.zip";
+            DownloadFile dwnDiag = new DownloadFile(fourKVD, url, oFile);
+            dwnDiag.ShowDialog();
+        }
+
+        private void BTN_WinRAR_Click(object sender, EventArgs e)
+        {
+            string oFile = "WinRAR_rarreg.key.zip";
+            string fourKVD = "WinRAR";
+            string url = "https://github.com/AlphaDECringee/softwarerepository/releases/download/WRARRARREG/WinRAR_rarreg.key.zip";
+            DownloadFile dwnDiag = new DownloadFile(fourKVD, url, oFile);
+            dwnDiag.ShowDialog();
+        }
     }
 }
